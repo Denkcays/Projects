3d19ca0d30e08e1a9882b602359f8f77ec0bb0a7
Denkcays <260854338+Denkcays@users.noreply.github.com>
2026-05-17 21:17:07 +0500

    Bug fixes

--- index.js
+++ index.js
@@ -19,25 +19,3 @@ const observer = new IntersectionObserver((entries) => {
 boxes.forEach(box => {
     observer.observe(box);
 });
-
-const foots = document.querySelectorAll('.foot');
-
-const web = new IntersectionObserver((entries) => {
-
-    entries.forEach(entry => {
-
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
-
-    });
-
-}, {
-    threshold: 0.2
-});
-
-foots.forEach(foot => {
-    web.observe(foot);
-});
